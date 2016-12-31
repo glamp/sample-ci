b2d514023537c5e911fb0ae3bababaae38d06032
Greg Lamp <lamp.greg@gmail.com>
2016-12-31 17:17:48 -0500

    IP

--- greg.py
+++ greg.py
@@ -4,7 +4,7 @@ from bandit import Bandit
 with open('output-files/stuff.txt', 'wb') as f:
     f.write("HI!")
 
-bandit = Bandit('glamp', 'fe69f312-cb65-11e6-9d5f-6c400889bca4', 'http://localhost:4567/')
+bandit = Bandit('glamp', 'fe69f312-cb65-11e6-9d5f-6c400889bca4', 'http://192.168.0.5:4567/')
 bandit.metadata.x = 1
 bandit.metadata.y2 = 0.83
 bandit.metadata.r2 = "hello!"
