52e22dc8336cb7d7fe32addc6d62e2b14b438ae6
Greg Lamp <lamp.greg@gmail.com>
2017-01-12 10:57:17 -0500

    reporting

--- greg.py
+++ greg.py
@@ -8,16 +8,16 @@ for i in range(250):
 # with open('output-files/stuff.txt', 'wb') as f:
 #    f.write("HI!")
 
-# bandit = Bandit()
+bandit = Bandit()
 # bandit.metadata.x = 1
 # bandit.metadata.y2 = 0.83
 # bandit.metadata.r2 = "hello!"
 
-# for x in range(10):
-#    for y in range(10):
-#        for tag in ["a", "b", "c", "d", "e", "f", "g"]:
-#            bandit.report(tag, y, random.normalvariate(0, 1))
-#        time.sleep(0.1)
+for x in range(10):
+    for y in range(10):
+        for tag in ["a", "b", "c", "d", "e", "f", "g"]:
+            bandit.report(tag, y, random.normalvariate(0, 1))
+            time.sleep(0.1)
 
 # email = job.Email("greg@yhathq.com", "This is a test email", "Hello self!")
 # email._write()
