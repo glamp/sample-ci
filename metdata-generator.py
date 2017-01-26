f3330a419fa02e7104968ca7ebe204e56317a516
Greg Lamp <lamp.greg@gmail.com>
2017-01-25 18:34:31 -0800

    metadata test

---
+++ metdata-generator.py
@@ -0,0 +1,15 @@
+from bandit import Bandit
+import random
+import math
+
+t0 = 1485397579.577891
+multiplier = math.exp((time.time() - t0) / (3*60*60))
+
+def noise():
+    return random.randrange(85, 100) / 100.
+
+bandit = Bandit()
+bandit.metadata.r2 = 0.35 * multiplier * noise()
+bandit.metadata.accuracy = 0.23 * multiplier * noise()
+bandit.metadata.precision = 0.32 * multiplier * noise()
+bandit.metadata.recall = 0.18 * multiplier * noise()
