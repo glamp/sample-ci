28d8c6ae8656978242e4ab25b3cde0ade8aae00c
Greg Lamp <lamp.greg@gmail.com>
2017-01-27 13:35:03 -0800

    rich email

--- email-me.py
+++ email-me.py
@@ -2,11 +2,17 @@ import time
 import random
 from bandit import Bandit
 import json
+import pandas as pd
+
+
+df = pd.DataFrame({ "x": range(10), "y": range(10) })
+
 
 email = {
     'recipients': ['greg@yhathq.com', 'colin@yhathq.com'],
     'subject': 'This is a test email',
-    'body': 'Hi Colin'
+    'body': '<h1>Hi Colin</h1>\n' + df.to_html(),
+    'isHTML': True
 }
 
 with open('/job/metadata/email.json', 'wb') as f:
