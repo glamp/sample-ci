478b9ec8f40432fadde6a46e9c776ab6097f1c5a
Greg Lamp <lamp.greg@gmail.com>
2017-01-27 13:26:48 -0800

    email job

---
+++ email-me.py
@@ -0,0 +1,14 @@
+import time
+import random
+from bandit import Bandit
+import json
+
+email = {
+    'recipients': 'greg@yhathq.com,colin@yhathq.com',
+    'subject': 'This is a test email',
+    'body': 'Hi Colin'
+}
+
+with open('/job/metadata/email.json', 'wb') as f:
+    f.write(json.dumps(email))
+
