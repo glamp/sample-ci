96e0e60bd98ab9a299eb0579dbbd4e0959b7e1b6
Greg Lamp <lamp.greg@gmail.com>
2017-01-27 14:41:39 -0800

    emailz

--- email-me.py
+++ email-me.py
@@ -10,6 +10,7 @@ df = pd.DataFrame({ "x": np.random.normal(10, 10, 10), "y": np.random.normal(10,
 
 
 email = Email()
-email.subject('This came from the client')
-email.body('<p>Colin <s>rules</s>sucks\n' + df.to_html())
+email.subject('plaintext email')
+# email.body('<p>Colin <s>rules</s>sucks\n' + df.to_html())
+email.body('but what if its plain')
 email.send(['greg@yhathq.com', 'colin@yhathq.com'])
