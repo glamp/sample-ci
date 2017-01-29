e20ccbb7f448ffefd7ca144fa83a414c078975f9
Greg Lamp <lamp.greg@gmail.com>
2017-01-29 15:34:00 -0800

    easy to test

--- email-me.py
+++ email-me.py
@@ -4,20 +4,20 @@ from bandit import Email
 import json
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-from ggplot import *
+# import matplotlib
+# matplotlib.use('Agg')
+# from ggplot import *
 
 
 df = pd.DataFrame({ "x": np.random.normal(10, 10, 10), "y": np.random.normal(10, 10, 10) })
 
-p = ggplot(meat, aes(x='date', y='beef')) + geom_line()
-p.save("./sample-image.png", width=10, height=10)
+# p = ggplot(meat, aes(x='date', y='beef')) + geom_line()
+# p.save("./sample-image.png", width=10, height=10)
 
 email = Email()
 email.subject('This came from the client')
 email.body('<p>Colin <s>rules</s>sucks\n' + df.to_html())
 df.to_csv('df.csv')
 email.add_attachment('df.csv')
-email.add_attachment('sample-image.png')
+email.add_attachment('sample-ggplot.png')
 email.send(['greg@yhathq.com', 'colin@yhathq.com'])
