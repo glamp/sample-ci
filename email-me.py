6287eb013f96bdc56e1ab84ee62d244744b0c89c
Greg Lamp <lamp.greg@gmail.com>
2017-01-29 14:30:37 -0800

    image example for email

--- email-me.py
+++ email-me.py
@@ -4,14 +4,18 @@ from bandit import Email
 import json
 import pandas as pd
 import numpy as np
+from ggplot import *
 
 
 df = pd.DataFrame({ "x": np.random.normal(10, 10, 10), "y": np.random.normal(10, 10, 10) })
 
+p = ggplot(meat, aes(x='date', y='beef')) + geom_line()
+p.save("./sample-image.png")
 
 email = Email()
 email.subject('This came from the client')
 email.body('<p>Colin <s>rules</s>sucks\n' + df.to_html())
 df.to_csv('df.csv')
 email.add_attachment('df.csv')
+email.add_attachment('sample-image.png')
 email.send(['greg@yhathq.com', 'colin@yhathq.com'])
