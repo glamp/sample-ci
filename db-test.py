bf5bccca6f752a7458370b7de8ae0626e2d476b9
Greg Lamp <lamp.greg@gmail.com>
2017-02-06 16:41:17 -0500

    Create db-test.py

---
+++ db-test.py
@@ -0,0 +1,19 @@
+from bandit import Bandit
+import psycopg2 as pg
+import pandas as pd
+
+bandit = Bandit()
+con = pg.connect(bandit.get_connection('ncaab'))
+
+sql = """
+select
+  *
+from
+  games g
+inner join
+  play_by_play p on
+    g.game_id = p.game_id;
+"""
+
+df = pd.read_sql_query(sql, con)
+print df.head()
