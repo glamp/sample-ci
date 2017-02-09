c283506cb4863e110e2bdef65c1ae773116b997a
Greg Lamp <lamp.greg@gmail.com>
2017-02-09 14:06:39 -0500

    test metrics

---
+++ make-metrics.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from bandit import Bandit
+import time
+
+
+df = pd.read_csv("./report-data.csv")
+for _, row in df.iterrows():
+    row = row.to_dict()
+    bandit.report(row['tag_name'], row['y'])
+    time.sleep(0.01)
