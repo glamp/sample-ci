bb65bd41371702ce97a256eac5d3b638b1f96990
Greg Lamp <lamp.greg@gmail.com>
2017-02-09 14:09:58 -0500

    bandito

--- make-metrics.py
+++ make-metrics.py
@@ -2,8 +2,9 @@ import pandas as pd
 from bandit import Bandit
 import time
 
-
+bandit = Bandit()
 df = pd.read_csv("./report-data.csv")
+
 for _, row in df.iterrows():
     row = row.to_dict()
     bandit.report(row['tag_name'], row['y'])
