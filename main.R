23716afb1ffb58f81efa5e46020df877d211cb61
Greg Lamp <lamp.greg@gmail.com>
2017-03-08 14:37:17 -0500

    email

--- main.R
+++ main.R
@@ -1,3 +1,11 @@
+library(banditr)
 
 
-print(rnorm(100))
+for (i in 1:100) {
+  stream("x", rnorm(1))
+}
+
+
+email("greg@yhathq.com", "This is a subject", "Ze body!")
+write.csv(iris, "./iris.csv", row.names=FALSE)
+add_attachment("./iris.csv")
